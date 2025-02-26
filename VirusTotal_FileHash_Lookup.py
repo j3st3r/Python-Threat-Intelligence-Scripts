0635a00ac9c0acb1f202241373373ee21e25eea6
Will Armijo <will.armijo@gmail.com>
2025-02-26 14:36:50 -0600

    Update VirusTotal_FileHash_Lookup.py

--- VirusTotal_FileHash_Lookup.py
+++ VirusTotal_FileHash_Lookup.py
@@ -6,10 +6,9 @@
 # Created on: 01/11/2025
 
 '''
-Please note that this script is not compliant with security as the API key is statically assigned. 
-This can be updated to retrieve the API key from a protected file, user input, or user environment variable. 
+Please note that this script requires an environment variable called API_KEY to be set prior to executing this script. 
 '''
-
+import os
 import requests
 import json
 import pandas as pd
@@ -22,7 +21,7 @@ from pandas import json_normalize
 
 # Comment the 'hash' variable out if you choose one of the hashes above.
 hash = input("Please enter a file hash: ")
-api_key = "<Replace this with your own API key from Virus Total>"
+api_key = os.environ.get('API_KEY') # Uses the enviornent variable called, API_KEY
 url = f"https://www.virustotal.com/api/v3/files/{hash}"
 
 # Set up the headers
